95ed6bbc6ec2453379909a047f6abc5483e4b87f
Asuma Yamada <yamadaasuma@gmail.com>
2021-12-06 19:58:53 +0900

    :bug: fix style bug

--- test-next-app/src/pages/_app.tsx
+++ test-next-app/src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    jssStyles?.parentElement?.removeChild(jssStyles);
+  }, []);
+
   return <Component {...pageProps} />
 }
 
