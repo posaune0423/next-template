19908d4125f4d51b76f2532fa220247dfc1962fc
Asuma Yamada <yamadaasuma@gmail.com>
2021-12-06 19:07:39 +0900

    :recycle: Refactoring

--- test-next-app/src/pages/_app.tsx
+++ test-next-app/src/pages/_app.tsx
@@ -1,15 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from '@mui/material'
-import { useDarkTheme } from 'hooks/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { theme } = useDarkTheme()
-  return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
