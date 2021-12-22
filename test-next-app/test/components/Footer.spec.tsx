537b14769dcda3c7e9da2307b24411952a64526b
Asuma Yamada <a.yamada@rakumo.com>
2021-12-22 13:07:03 +0900

    feat: add jest for test

---
+++ test-next-app/test/components/Footer.spec.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react'
+import { Footer } from '../../src/components/footer'
+
+describe('testing', () => {
+  it('if Footer component is correctly rendered', () => {
+    const { asFragment } = render(<Footer />)
+    expect(asFragment()).toMatchSnapshot()
+  })
+})
